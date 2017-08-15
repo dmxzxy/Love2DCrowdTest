95a3e93c38635d07bcc11c0fc1b1243776e76097
xiaoyu.zhang <xiaoyu.zhang@dena.com>
2017-08-15 11:05:27 +0800

    update

--- xlstolua/plugins/gen_lua.py
+++ xlstolua/plugins/gen_lua.py
@@ -183,19 +183,28 @@ def code_gen_datas(data_desc, config_desc):
 
     return context.getvalue()
 
+def code_gen_config(config_desc):
+    context = Writer()
+    write_header(context)
+    context('local %s = {\n'%config_desc.name)
+
+    _datas = [];
+    for (k,v) in config_desc.attr_datas.items():
+        _datas.append(code_gen_datas(v, config_desc))
+
+    map(context, _datas)
+    context('}\nreturn %s\n'%config_desc.name)
+    return context.getvalue();
+
+
 def code_gen_file(file_desc):
+    export_file_names = {};
     for config in file_desc.configs:
-        context = Writer()
-        write_header(context)
-        context('local %s = {\n'%config.name)
+        context_value = code_gen_config(config)
+        export_file_names[config.name + '.lua'] = context_value
 
-        _datas = [];
-        for (k,v) in config.attr_datas.items():
-            _datas.append(code_gen_datas(v, config))
-
-        map(context, _datas)
-        context('}\nreturn %s\n'%config.name)
-        _files[config.name + '.lua'] = context.getvalue()
+    for (k,v) in export_file_names.items():
+        _files[k] = v
 
 def gen_code(req, toPath):
     print '.........start gen type : '+type_name()+'..........'
